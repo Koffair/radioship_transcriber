6481ea583fcc32aedd24169e8de8387928388a03
partvishegy <venczeli.jozsef@gmail.com>
2023-05-16 17:04:53 +0200

    moved to argparse, model loads

--- src/transcript_tool.py
+++ src/transcript_tool.py
@@ -1,48 +1,91 @@
-import click
-import os
+"""This is the main module of the transctipter tool.
 
-# --- Notes ---- 
-# create main function pseudo code
-# Click interface in action
-# talk to Zoli about the model usage
-# check check if hooks are working
+The tool requeires an input and an ouput folder to be specified.
+The input folder should contain mp3 files. 
+
+There is a default model path provided that can be replaced."""
+
+import os
+import argparse
+
+from huggingsound import SpeechRecognitionModel  # type: ignore
+
+
+# --- Notes ----
+# check check if hooks are working - DONE. yes.
+# do not forget typehints - DONE: mypy working.
+
+# do not forget logging
+# docstrings
+# tests
+# feature-branch
 # --------------
 
 
-default_model = "[addres of default model]"
+DEFAULT_MODEL = "../models/final_model-20230516T143657Z-001/final_model/"
+
+parser = argparse.ArgumentParser("Create transript for mp3 files.")
+parser.add_argument(
+    "-i",
+    "--in_path",
+    type=str,
+    metavar="",
+    required=True,
+    help="Path to input file or directory",
+)
+parser.add_argument(
+    "-o",
+    "--out_path",
+    type=str,
+    metavar="",
+    required=True,
+    help="Path to output directory",
+)
+parser.add_argument(
+    "-m",
+    "--model_path",
+    type=str,
+    metavar="",
+    default=DEFAULT_MODEL,
+    required=False,
+    help="Address to transcripter model",
+)
+args = parser.parse_args()
 
 
-@click.command()
-@click.option("--in_path", "-i",  help="Path to input file or directory")
-@click.option("--out_path", "-o",  help="Path to output directory")
-@click.option("--model", "-m",  default=default_model, help="Address to transripter model",)
-def make_transcript(in_path, out_path, model):
-
-    # create output folder if does not exist (ask maybe...)
-    # asking only makes sense for the cli, not the callable... get it outside.
+def main(in_path: str, out_path: str, model_path: str) -> None:
+    """This is the entry point for the CLI of the radioship transcript tool."""
     if not os.path.isdir(out_path):
-       print("Output dir [{}] does not exist!".format(out_path))
-       create_q = input("Would you like to create it? y/n: ")
-       if create_q == "y":
-           os.mkdir(out_path)
-           print("Output dir [{}] created!\n".format(out_path))
-       else:
-           return
+        print(f"Output dir [{out_path}] does not exist!")
+        create_q = input("Would you like to create it? y/n: ")
+        if create_q == "y":
+            os.mkdir(out_path)
+            print(f"Output dir [{out_path}] created!\n")
+        else:
+            return
 
-    # fetch model 
-    print("\nModel: {} fetched\n".format(model))
+    # fetch model
+    model = SpeechRecognitionModel(model_path)
+    print(f"\nModel: {model_path} fetched\n")
 
-    print("Creating transcript for files in {}. \nOutput dir: {}".format(in_path, out_path))
-    # get filesr_list
+    print(f"Creating transcript for files in {in_path}. \nOutput dir: {out_path}")
+    # get file_list
     input_files = os.listdir(in_path)
-    for f in input_files:
-        if os.path.isfile(f):
+    for audio_file in input_files:
+        if os.path.isfile(audio_file):
             # create transcript
-            print("transcipt created for {}".format(f))
+            make_transcript(audio_file, out_path, model)
+            print(f"transcipt created for {audio_file}")
 
 
+def make_transcript(audio_file: str, out_path: str, model) -> None: # what is the model type?
+    """Creates a transcirpt for a provided mp3 audio segment."""
 
-if __name__ == '__main__':
+    # model. valami predict
+    # see transcription part in Zoli script
 
-    make_transcript()
 
+if __name__ == "__main__":
+    main(
+        args.in_path, args.out_path, args.model_path
+    )  # pylint: disable=no-value-for-parameter

60d1a3d2d864ef48c7820ba40cfd045ddf5f9785
partvishegy <venczeli.jozsef@gmail.com>
2023-09-15 17:06:20 +0200

    fix entry point in setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     description="Interface and wrapper to run radioship_transcriber",
     entry_points={
         "console_scripts": [
-            "radioship_transcriber=radioship_transcriber.cli:main",
+            "radioship_transcriber=radioship_transcriber.cli.__main__:main",
         ],
     },
     install_requires=requirements,
